bccc5704c602d1c5f44e7cd0cdb3245266c5d0e9
zchaodc <zchaodc@hotmail.com>
2015-07-01 22:37:25 +0800

    Modified mac_bashrc and install vim script

--- vim/vim_install.sh
+++ vim/vim_install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+VimPath=".vim"
+BundlePath="${VimPath}/bundle"
+
 # home dir
 cd ~
 # check the .vim dir
 if [[ ! -d .vim ]]; then
-  mkdir -pv .vim
+  \mkdir -pv .vim
 fi
 
 # install vundle
+if [[ ! -d ${BundlePath} ]]; then
+    \mkdir -pv ${BundlePath}
+fi
+
+# Set up Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # copy defaulf vimrc
+# cp ./tools/myconf/vim
