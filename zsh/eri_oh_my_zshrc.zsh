2718427f50dbb94e8ceb6218dc4a30cb46b15253
Chao Zhao C <chao.c.zhao@ericsson.com>
2015-09-11 11:25:40 +0200

    Added tty settings
    Solved issue:
    
    Ctrl+S hang up the vim
    
    use Ctrl+Q to resume
    
    Feature Software Flow Control (XON/XOFF flow control)
    
    http://unix.stackexchange.com/questions/72086/ctrl-s-hang-terminal-emulator

--- zsh/eri_oh_my_zshrc.zsh
+++ zsh/eri_oh_my_zshrc.zsh
@@ -85,6 +85,9 @@ export EDITOR='vim'
 ### settings for man(manual page)
 # export MANWIDTH=80
 
+# tty settings
+stty -ixon
+
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
--- zsh/mac_oh_my_zshrc.zsh
+++ zsh/mac_oh_my_zshrc.zsh
@@ -79,6 +79,9 @@ export ARCHFLAGS="-arch x86_64"
 # ssh
 export SSH_KEY_PATH="~/.ssh/dsa_id"
 
+# tty settings
+stty -ixon
+
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
